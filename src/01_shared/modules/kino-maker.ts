af2eb8c525b5a9fca03897db6d5a20b1a6132606
Jamal Hasuev <jhasuev@mail.ru>
2023-11-20 14:54:41 +0300

    init + add some logic

---
+++ src/01_shared/modules/kino-maker.ts
@@ -0,0 +1,75 @@
+interface IParams {
+  container: string
+  url: string
+  onupdate: Function|null
+}
+interface IStatus {
+  currentTime: number
+  percent: number
+  duration: number
+}
+interface IAnimateItem {
+  showAt: number
+  hideAt: number
+  block: any
+}
+
+export default class KinoMaker {
+  params: IParams
+  videoTag: HTMLVideoElement
+  status: IStatus
+  animates: IAnimateItem[]
+
+  constructor(params: IParams, animates: IAnimateItem[] = []) {
+    this.params = params
+    this.status = { currentTime: 0, percent: 0, duration: 0 }
+    this.animates = animates.reduce(
+      (acc: IAnimateItem[], item: IAnimateItem, i: number) =>
+      {
+        return [...acc, { ...item, id: i + 1 }]
+      },
+      []
+    )
+  }
+
+  init() {
+    const videoContainer = document.querySelector(this.params.container) as HTMLDivElement
+    this.videoTag = document.createElement('video') as HTMLVideoElement
+
+    this.videoTag.addEventListener("timeupdate", () => {
+      this.updateStatus()
+      this.updateContent()
+    })
+
+    
+    this.videoTag.setAttribute('src', this.params.url)
+    this.videoTag.setAttribute('style', `width: 100%`)
+    videoContainer.appendChild(this.videoTag)
+  }
+
+  updateStatus() {
+    this.status.currentTime = this.videoTag.currentTime
+    this.status.duration = this.videoTag.duration
+    this.status.percent = this.status.currentTime / this.status.duration * 100
+
+    this.params.onupdate?.(this.status)
+  }
+
+  updateContent() {
+    const animatesForShowing = this.animates.filter((animate: IAnimateItem) => {
+      return this.status.currentTime >= animate.showAt && this.status.currentTime <= animate.hideAt
+    })
+  }
+
+  start() {
+    this.videoTag.play?.()
+  }
+
+  stop() {
+    this.videoTag.pause?.()
+  }
+
+  unmount() {
+    this.videoTag.play?.()
+  }
+}
---
+++ src/05_pages/home/ui.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount } from 'vue'
+import KinoMaker from '@/01_shared/modules/kino-maker'
+import videpMp4 from '@/01_shared/assets/videos/production_id_3981776_240p.mp4'
+
+defineOptions({ name: 'home' })
+
+const onStatusUpdate = (statusData: any) => {
+  // console.log(statusData);
+}
+
+const kinoMaker = new KinoMaker({
+  container: '.video-wrapper',
+  url: videpMp4,
+  onupdate: onStatusUpdate
+}, [
+  {
+    showAt: 1,
+    hideAt: 9,
+    block: `<div class="block-1" style="border: 10px solid; color: orange; font-size: 30px; padding: 50px;">some block</div>`,
+  },
+  {
+    showAt: 5,
+    hideAt: 6,
+    block: `<div class="block-2" style="border: 10px solid; color: blue; font-size: 30px; padding: 50px;">fast block</div>`,
+  },
+])
+
+onMounted(() => {
+  kinoMaker.init()
+})
+onBeforeUnmount(() => {
+  kinoMaker.unmount()
+})
+
+</script>
+
+<template>
+  <div>
+    video-wrapper:
+    <button @click="kinoMaker.start()">start</button>
+    <button @click="kinoMaker.stop()">stop</button>
+    <div class="video-wrapper"></div>
+  </div>
+</template>
